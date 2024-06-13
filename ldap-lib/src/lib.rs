5c341a4ea220f51f194dcad52f5168d909fc6c25
Jan-Eric Hellenberg <janeric.hellenberg@gmail.com>
2024-06-13 12:25:49 +0200

    Actually enforce that warnings are denied

--- ldap-lib/src/lib.rs
+++ ldap-lib/src/lib.rs
@@ -1,6 +1,6 @@
+#![deny(warnings)]
+#![deny(clippy::all)]
 #![cfg_attr(test, allow(dead_code, non_snake_case))]
-#[deny(warnings)]
-#[deny(clippy::all)]
 pub mod entry;
 mod keycloak_service_account;
 mod proto;
