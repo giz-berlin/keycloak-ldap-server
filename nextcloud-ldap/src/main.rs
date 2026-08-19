d29e9c873f882b69740ae6d55cdf69aaebaf6c71
Leonard Marschke <leonard@marschke.me>
2026-08-19 21:46:06 +0200

    fix: include subgroup members in nextcloud group memberships like Keycloak does with roles as Nextcloud has no hierarchical group tree

--- nextcloud-ldap/src/main.rs
+++ nextcloud-ldap/src/main.rs
@@ -46,5 +46,5 @@ impl giz_ldap_lib::interface::Target for Target {
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    server::start_ldap_server::<Target>(giz_ldap_lib::constants::GroupStrategy::DirectMembers).await
+    server::start_ldap_server::<Target>(giz_ldap_lib::constants::GroupStrategy::SubgroupMembers).await
 }
